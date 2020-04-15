1d7b2abc535be5eb9a3a77890459ade6274e912b
miaoski <lin.zhemin@gmail.com>
2020-04-15 03:54:47 +0200

    use another docker-compose file for srsenb

--- srslte/srsenb_init.sh
+++ srslte/srsenb_init.sh
@@ -2,9 +2,6 @@
 
 mkdir -p /etc/srslte
 cp /srsLTE/*.conf /etc/srslte/
-set -a
-. /mnt/srsenb/.env
-set +a
 
 sed -i 's|MME_IP|'$MME_IP'|g' /etc/srslte/enb.conf
 sed -i 's|ENB_IP|'$ENB_IP'|g' /etc/srslte/enb.conf

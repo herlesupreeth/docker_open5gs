df1b7fa1f293f2ea38bc27a1d4e4bbe9f34f860c
herlesupreeth <herlesupreeth@gmail.com>
2021-03-06 15:42:41 +0530

    Use freediameter conf file for mme

--- mme/mme_init.sh
+++ mme/mme_init.sh
@@ -32,14 +32,22 @@ export IF_NAME=$(ip r | awk '/default/ { print $5 }')
 [ ${#MNC} == 3 ] && EPC_DOMAIN="epc.mnc${MNC}.mcc${MCC}.3gppnetwork.org" || EPC_DOMAIN="epc.mnc0${MNC}.mcc${MCC}.3gppnetwork.org"
 
 cp /mnt/mme/mme.yaml install/etc/open5gs
+cp /mnt/mme/mme.conf install/etc/freeDiameter
+cp /mnt/mme/make_certs.sh install/etc/freeDiameter
+
 sed -i 's|MNC|'$MNC'|g' install/etc/open5gs/mme.yaml
 sed -i 's|MCC|'$MCC'|g' install/etc/open5gs/mme.yaml
-sed -i 's|MME_IP|'$IP_ADDR'|g' install/etc/open5gs/mme.yaml
+sed -i 's|MME_IP|'$MME_IP'|g' install/etc/open5gs/mme.yaml
 sed -i 's|MME_IF|'$IF_NAME'|g' install/etc/open5gs/mme.yaml
-sed -i 's|HSS_IP|'$HSS_IP'|g' install/etc/open5gs/mme.yaml
 sed -i 's|SGWC_IP|'$SGWC_IP'|g' install/etc/open5gs/mme.yaml
 sed -i 's|SMF_IP|'$SMF_IP'|g' install/etc/open5gs/mme.yaml
-sed -i 's|EPC_DOMAIN|'$EPC_DOMAIN'|g' install/etc/open5gs/mme.yaml
+sed -i 's|MME_IP|'$MME_IP'|g' install/etc/freeDiameter/mme.conf
+sed -i 's|HSS_IP|'$HSS_IP'|g' install/etc/freeDiameter/mme.conf
+sed -i 's|EPC_DOMAIN|'$EPC_DOMAIN'|g' install/etc/freeDiameter/mme.conf
+sed -i 's|EPC_DOMAIN|'$EPC_DOMAIN'|g' install/etc/freeDiameter/make_certs.sh
+
+# Generate TLS certificates
+./install/etc/freeDiameter/make_certs.sh install/etc/freeDiameter
 
 # Sync docker time
 #ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone

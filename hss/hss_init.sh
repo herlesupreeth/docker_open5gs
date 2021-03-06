13c3dc0326412a30d3d58f90d431ed05df70d3bf
herlesupreeth <herlesupreeth@gmail.com>
2021-03-06 15:41:07 +0530

    Use freediameter conf file for hss

--- hss/hss_init.sh
+++ hss/hss_init.sh
@@ -33,10 +33,17 @@ export IP_ADDR=$(awk 'END{print $1}' /etc/hosts)
 [ ${#MNC} == 3 ] && EPC_DOMAIN="epc.mnc${MNC}.mcc${MCC}.3gppnetwork.org" || EPC_DOMAIN="epc.mnc0${MNC}.mcc${MCC}.3gppnetwork.org"
 
 cp /mnt/hss/hss.yaml install/etc/open5gs
-sed -i 's|HSS_IP|'$IP_ADDR'|g' install/etc/open5gs/hss.yaml
-sed -i 's|MME_IP|'$MME_IP'|g' install/etc/open5gs/hss.yaml
+cp /mnt/hss/hss.conf install/etc/freeDiameter
+cp /mnt/hss/make_certs.sh install/etc/freeDiameter
+
+sed -i 's|HSS_IP|'$HSS_IP'|g' install/etc/freeDiameter/hss.conf
+sed -i 's|MME_IP|'$MME_IP'|g' install/etc/freeDiameter/hss.conf
+sed -i 's|EPC_DOMAIN|'$EPC_DOMAIN'|g' install/etc/freeDiameter/hss.conf
+sed -i 's|EPC_DOMAIN|'$EPC_DOMAIN'|g' install/etc/freeDiameter/make_certs.sh
 sed -i 's|MONGO_IP|'$MONGO_IP'|g' install/etc/open5gs/hss.yaml
-sed -i 's|EPC_DOMAIN|'$EPC_DOMAIN'|g' install/etc/open5gs/hss.yaml
+
+# Generate TLS certificates
+./install/etc/freeDiameter/make_certs.sh install/etc/freeDiameter
 
 # Sync docker time
 #ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone

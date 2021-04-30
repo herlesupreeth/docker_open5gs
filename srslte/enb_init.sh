87676277421d2b06a6676282ed17dedc163d3f2f
herlesupreeth <herlesupreeth@gmail.com>
2021-04-30 14:24:52 +0200

    srsRAN adaptations and use the latest srsRAN code for eNB

--- srslte/enb_init.sh
+++ srslte/enb_init.sh
@@ -28,18 +28,20 @@
 
 export IP_ADDR=$(awk 'END{print $1}' /etc/hosts)
 
-cp /mnt/srslte/enb.conf /etc/srslte
-cp /mnt/srslte/drb.conf /etc/srslte
-cp /mnt/srslte/epc.conf /etc/srslte
-cp /mnt/srslte/mbms.conf /etc/srslte
-cp /mnt/srslte/rr.conf /etc/srslte
-cp /mnt/srslte/sib.conf /etc/srslte
-cp /mnt/srslte/ue.conf /etc/srslte
-cp /mnt/srslte/user_db.csv /etc/srslte
-sed -i 's|MNC|'$MNC'|g' /etc/srslte/enb.conf
-sed -i 's|MCC|'$MCC'|g' /etc/srslte/enb.conf
-sed -i 's|MME_IP|'$MME_IP'|g' /etc/srslte/enb.conf
-sed -i 's|SRS_ENB_IP|'$SRS_ENB_IP'|g' /etc/srslte/enb.conf
+mkdir -p /etc/srsran
+
+cp /mnt/srslte/enb.conf /etc/srsran
+cp /mnt/srslte/drb.conf /etc/srsran
+cp /mnt/srslte/epc.conf /etc/srsran
+cp /mnt/srslte/mbms.conf /etc/srsran
+cp /mnt/srslte/rr.conf /etc/srsran
+cp /mnt/srslte/sib.conf /etc/srsran
+cp /mnt/srslte/ue.conf /etc/srsran
+cp /mnt/srslte/user_db.csv /etc/srsran
+sed -i 's|MNC|'$MNC'|g' /etc/srsran/enb.conf
+sed -i 's|MCC|'$MCC'|g' /etc/srsran/enb.conf
+sed -i 's|MME_IP|'$MME_IP'|g' /etc/srsran/enb.conf
+sed -i 's|SRS_ENB_IP|'$SRS_ENB_IP'|g' /etc/srsran/enb.conf
 
 # Sync docker time
 #ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone

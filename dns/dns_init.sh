75380d7b1dcb03dff8ed7b86f35daa1a5a95c547
herlesupreeth <herlesupreeth@gmail.com>
2022-01-30 13:05:44 +0100

    Initial work on entitlement server addition to docker setup

--- dns/dns_init.sh
+++ dns/dns_init.sh
@@ -28,10 +28,12 @@
 
 cp /mnt/dns/epc_zone /etc/bind
 cp /mnt/dns/ims_zone /etc/bind
+cp /mnt/dns/pub_3gpp_zone /etc/bind
 cp /mnt/dns/named.conf /etc/bind
 
 [ ${#MNC} == 3 ] && EPC_DOMAIN="epc.mnc${MNC}.mcc${MCC}.3gppnetwork.org" || EPC_DOMAIN="epc.mnc0${MNC}.mcc${MCC}.3gppnetwork.org"
 [ ${#MNC} == 3 ] && IMS_DOMAIN="ims.mnc${MNC}.mcc${MCC}.3gppnetwork.org" || IMS_DOMAIN="ims.mnc0${MNC}.mcc${MCC}.3gppnetwork.org"
+[ ${#MNC} == 3 ] && PUB_3GPP_DOMAIN="mnc${MNC}.mcc${MCC}.pub.3gppnetwork.org" || PUB_3GPP_DOMAIN="mnc0${MNC}.mcc${MCC}.pub.3gppnetwork.org"
 
 sed -i 's|EPC_DOMAIN|'$EPC_DOMAIN'|g' /etc/bind/epc_zone
 sed -i 's|DNS_IP|'$DNS_IP'|g' /etc/bind/epc_zone
@@ -44,8 +46,13 @@ sed -i 's|ICSCF_IP|'$ICSCF_IP'|g' /etc/bind/ims_zone
 sed -i 's|SCSCF_IP|'$SCSCF_IP'|g' /etc/bind/ims_zone
 sed -i 's|FHOSS_IP|'$FHOSS_IP'|g' /etc/bind/ims_zone
 
+sed -i 's|PUB_3GPP_DOMAIN|'$PUB_3GPP_DOMAIN'|g' /etc/bind/pub_3gpp_zone
+sed -i 's|DNS_IP|'$DNS_IP'|g' /etc/bind/pub_3gpp_zone
+sed -i 's|ENTITLEMENT_SERVER_IP|'$ENTITLEMENT_SERVER_IP'|g' /etc/bind/pub_3gpp_zone
+
 sed -i 's|EPC_DOMAIN|'$EPC_DOMAIN'|g' /etc/bind/named.conf
 sed -i 's|IMS_DOMAIN|'$IMS_DOMAIN'|g' /etc/bind/named.conf
+sed -i 's|PUB_3GPP_DOMAIN|'$PUB_3GPP_DOMAIN'|g' /etc/bind/named.conf
 
 # Sync docker time
 #ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone

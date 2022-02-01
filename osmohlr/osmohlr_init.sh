4868af9ccd8980cf735bae90e983fce7cfd7e083
herlesupreeth <herlesupreeth@gmail.com>
2022-02-01 11:28:54 +0100

    Add support for SMS over SGs interface

--- osmohlr/osmohlr_init.sh
+++ osmohlr/osmohlr_init.sh
@@ -26,29 +26,9 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-export IP_ADDR=$(awk 'END{print $1}' /etc/hosts)
-export IF_NAME=$(ip r | awk '/default/ { print $5 }')
+cp /mnt/osmohlr/osmo-hlr.cfg /etc/osmocom
 
-[ ${#MNC} == 3 ] && EPC_DOMAIN="epc.mnc${MNC}.mcc${MCC}.3gppnetwork.org" || EPC_DOMAIN="epc.mnc0${MNC}.mcc${MCC}.3gppnetwork.org"
-
-cp /mnt/mme/mme.yaml install/etc/open5gs
-cp /mnt/mme/mme.conf install/etc/freeDiameter
-cp /mnt/mme/make_certs.sh install/etc/freeDiameter
-
-sed -i 's|MNC|'$MNC'|g' install/etc/open5gs/mme.yaml
-sed -i 's|MCC|'$MCC'|g' install/etc/open5gs/mme.yaml
-sed -i 's|MME_IP|'$MME_IP'|g' install/etc/open5gs/mme.yaml
-sed -i 's|MME_IF|'$IF_NAME'|g' install/etc/open5gs/mme.yaml
-sed -i 's|OSMOMSC_IP|'$OSMOMSC_IP'|g' install/etc/open5gs/mme.yaml
-sed -i 's|SGWC_IP|'$SGWC_IP'|g' install/etc/open5gs/mme.yaml
-sed -i 's|SMF_IP|'$SMF_IP'|g' install/etc/open5gs/mme.yaml
-sed -i 's|MME_IP|'$MME_IP'|g' install/etc/freeDiameter/mme.conf
-sed -i 's|HSS_IP|'$HSS_IP'|g' install/etc/freeDiameter/mme.conf
-sed -i 's|EPC_DOMAIN|'$EPC_DOMAIN'|g' install/etc/freeDiameter/mme.conf
-sed -i 's|EPC_DOMAIN|'$EPC_DOMAIN'|g' install/etc/freeDiameter/make_certs.sh
-
-# Generate TLS certificates
-./install/etc/freeDiameter/make_certs.sh install/etc/freeDiameter
+sed -i 's|OSMOHLR_IP|'$OSMOHLR_IP'|g' /etc/osmocom/osmo-hlr.cfg
 
 # Sync docker time
 #ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
--- osmomsc/osmomsc_init.sh
+++ osmomsc/osmomsc_init.sh
@@ -26,29 +26,15 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-export IP_ADDR=$(awk 'END{print $1}' /etc/hosts)
-export IF_NAME=$(ip r | awk '/default/ { print $5 }')
+[ ${#MNC} == 3 ] && 3GPP_REALM="mnc${MNC}.mcc${MCC}.3gppnetwork.org" || 3GPP_REALM="mnc0${MNC}.mcc${MCC}.3gppnetwork.org"
 
-[ ${#MNC} == 3 ] && EPC_DOMAIN="epc.mnc${MNC}.mcc${MCC}.3gppnetwork.org" || EPC_DOMAIN="epc.mnc0${MNC}.mcc${MCC}.3gppnetwork.org"
+cp /mnt/osmomsc/osmo-msc.cfg /etc/osmocom
 
-cp /mnt/mme/mme.yaml install/etc/open5gs
-cp /mnt/mme/mme.conf install/etc/freeDiameter
-cp /mnt/mme/make_certs.sh install/etc/freeDiameter
-
-sed -i 's|MNC|'$MNC'|g' install/etc/open5gs/mme.yaml
-sed -i 's|MCC|'$MCC'|g' install/etc/open5gs/mme.yaml
-sed -i 's|MME_IP|'$MME_IP'|g' install/etc/open5gs/mme.yaml
-sed -i 's|MME_IF|'$IF_NAME'|g' install/etc/open5gs/mme.yaml
-sed -i 's|OSMOMSC_IP|'$OSMOMSC_IP'|g' install/etc/open5gs/mme.yaml
-sed -i 's|SGWC_IP|'$SGWC_IP'|g' install/etc/open5gs/mme.yaml
-sed -i 's|SMF_IP|'$SMF_IP'|g' install/etc/open5gs/mme.yaml
-sed -i 's|MME_IP|'$MME_IP'|g' install/etc/freeDiameter/mme.conf
-sed -i 's|HSS_IP|'$HSS_IP'|g' install/etc/freeDiameter/mme.conf
-sed -i 's|EPC_DOMAIN|'$EPC_DOMAIN'|g' install/etc/freeDiameter/mme.conf
-sed -i 's|EPC_DOMAIN|'$EPC_DOMAIN'|g' install/etc/freeDiameter/make_certs.sh
-
-# Generate TLS certificates
-./install/etc/freeDiameter/make_certs.sh install/etc/freeDiameter
+sed -i 's|OSMOMSC_IP|'$OSMOMSC_IP'|g' /etc/osmocom/osmo-msc.cfg
+sed -i 's|OSMOHLR_IP|'$OSMOHLR_IP'|g' /etc/osmocom/osmo-msc.cfg
+sed -i 's|MNC|'$MNC'|g' /etc/osmocom/osmo-msc.cfg
+sed -i 's|MCC|'$MCC'|g' /etc/osmocom/osmo-msc.cfg
+sed -i 's|3GPP_REALM|'$3GPP_REALM'|g' /etc/osmocom/osmo-msc.cfg
 
 # Sync docker time
 #ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone

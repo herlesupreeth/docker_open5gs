3b21289d2f0eacd4b7209acd26fb46273c60e80e
herlesupreeth <herlesupreeth@gmail.com>
2022-09-16 09:40:35 +0200

    Introduce srsRAN ZMQ mode (eNB, gNB, 4G UE, 5G UE)

--- srslte/srslte_init.sh
+++ srslte/srslte_init.sh
@@ -30,20 +30,37 @@ export IP_ADDR=$(awk 'END{print $1}' /etc/hosts)
 
 mkdir -p /etc/srsran
 
-cp /mnt/srslte/enb.conf /etc/srsran
 cp /mnt/srslte/rb.conf /etc/srsran
 cp /mnt/srslte/sib.conf /etc/srsran
 
 if [[ -z "$COMPONENT_NAME" ]]; then
 	echo "Error: COMPONENT_NAME environment variable not set"; exit 1;
 elif [[ "$COMPONENT_NAME" =~ ^(gnb$) ]]; then
-	echo "Deploying component: '$COMPONENT_NAME'"
+	echo "Configuring component: '$COMPONENT_NAME'"
 	cp /mnt/srslte/rr_gnb.conf /etc/srsran/rr.conf
+	cp /mnt/srslte/enb.conf /etc/srsran
 	sed -i 's|MME_IP|'$AMF_IP'|g' /etc/srsran/enb.conf
 elif [[ "$COMPONENT_NAME" =~ ^(enb$) ]]; then
-	echo "Deploying component: '$COMPONENT_NAME'"
+	echo "Configuring component: '$COMPONENT_NAME'"
 	cp /mnt/srslte/rr.conf /etc/srsran
+	cp /mnt/srslte/enb.conf /etc/srsran
 	sed -i 's|MME_IP|'$MME_IP'|g' /etc/srsran/enb.conf
+elif [[ "$COMPONENT_NAME" =~ ^(enb_zmq$) ]]; then
+	echo "Configuring component: '$COMPONENT_NAME'"
+	cp /mnt/srslte/rr.conf /etc/srsran
+	cp /mnt/srslte/enb_zmq.conf /etc/srsran/enb.conf
+	sed -i 's|MME_IP|'$MME_IP'|g' /etc/srsran/enb.conf
+elif [[ "$COMPONENT_NAME" =~ ^(gnb_zmq$) ]]; then
+	echo "Configuring component: '$COMPONENT_NAME'"
+	cp /mnt/srslte/rr_gnb.conf /etc/srsran/rr.conf
+	cp /mnt/srslte/enb_zmq.conf /etc/srsran/enb.conf
+	sed -i 's|MME_IP|'$AMF_IP'|g' /etc/srsran/enb.conf
+elif [[ "$COMPONENT_NAME" =~ ^(ue_zmq$) ]]; then
+	echo "Configuring component: '$COMPONENT_NAME'"
+	cp /mnt/srslte/ue_zmq.conf /etc/srsran/ue.conf
+elif [[ "$COMPONENT_NAME" =~ ^(ue_5g_zmq$) ]]; then
+	echo "Configuring component: '$COMPONENT_NAME'"
+	cp /mnt/srslte/ue_5g_zmq.conf /etc/srsran/ue.conf
 else
 	echo "Error: Invalid component name: '$COMPONENT_NAME'"
 fi
@@ -51,8 +68,27 @@ fi
 sed -i 's|MNC|'$MNC'|g' /etc/srsran/enb.conf
 sed -i 's|MCC|'$MCC'|g' /etc/srsran/enb.conf
 sed -i 's|SRS_ENB_IP|'$SRS_ENB_IP'|g' /etc/srsran/enb.conf
+sed -i 's|SRS_UE_IP|'$SRS_UE_IP'|g' /etc/srsran/enb.conf
+sed -i 's|UE1_KI|'$UE1_KI'|g' /etc/srsran/ue.conf
+sed -i 's|UE1_OP|'$UE1_OP'|g' /etc/srsran/ue.conf
+sed -i 's|UE1_IMSI|'$UE1_IMSI'|g' /etc/srsran/ue.conf
+sed -i 's|SRS_UE_IP|'$SRS_UE_IP'|g' /etc/srsran/ue.conf
+sed -i 's|SRS_ENB_IP|'$SRS_ENB_IP'|g' /etc/srsran/ue.conf
 
+# For dbus not started issue when host machine is running Ubuntu 22.04
 service dbus start && service avahi-daemon start
+
+if [[ -z "$COMPONENT_NAME" ]]; then
+	echo "Error: COMPONENT_NAME environment variable not set"; exit 1;
+elif [[ "$COMPONENT_NAME" =~ ^(gnb$) || "$COMPONENT_NAME" =~ ^(enb$) || "$COMPONENT_NAME" =~ ^(enb_zmq$) || "$COMPONENT_NAME" =~ ^(gnb_zmq$) ]]; then
+	echo "Deploying component: '$COMPONENT_NAME'"
+	/usr/local/bin/srsenb
+elif [[ "$COMPONENT_NAME" =~ ^(ue_zmq$) || "$COMPONENT_NAME" =~ ^(ue_5g_zmq$) ]]; then
+	echo "Deploying component: '$COMPONENT_NAME'"
+	/usr/local/bin/srsue
+else
+	echo "Error: Invalid component name: '$COMPONENT_NAME'"
+fi
 
 # Sync docker time
 #ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone

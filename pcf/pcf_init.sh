6df1c4dfd0b1e9a519efee06de196038b1dc1a89
herlesupreeth <herlesupreeth@gmail.com>
2022-10-28 14:46:42 +0200

    Add SCP to deployment

--- pcf/pcf_init.sh
+++ pcf/pcf_init.sh
@@ -30,7 +30,7 @@ export DB_URI="mongodb://${MONGO_IP}/open5gs"
 
 cp /mnt/pcf/pcf.yaml install/etc/open5gs
 sed -i 's|PCF_IP|'$PCF_IP'|g' install/etc/open5gs/pcf.yaml
-sed -i 's|NRF_IP|'$NRF_IP'|g' install/etc/open5gs/pcf.yaml
+sed -i 's|SCP_IP|'$SCP_IP'|g' install/etc/open5gs/pcf.yaml
 sed -i 's|MONGO_IP|'$MONGO_IP'|g' install/etc/open5gs/pcf.yaml
 
 # Sync docker time
--- scp/scp_init.sh
+++ scp/scp_init.sh
@@ -26,9 +26,9 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-cp /mnt/udm/udm.yaml install/etc/open5gs
-sed -i 's|UDM_IP|'$UDM_IP'|g' install/etc/open5gs/udm.yaml
-sed -i 's|SCP_IP|'$SCP_IP'|g' install/etc/open5gs/udm.yaml
+cp /mnt/scp/scp.yaml install/etc/open5gs
+sed -i 's|SCP_IP|'$SCP_IP'|g' install/etc/open5gs/scp.yaml
+sed -i 's|NRF_IP|'$NRF_IP'|g' install/etc/open5gs/scp.yaml
 
 # Sync docker time
 #ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone

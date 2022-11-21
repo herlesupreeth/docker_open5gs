eee2366977490ce50363d202879878e115dfeae3
herlesupreeth <herlesupreeth@gmail.com>
2022-11-21 09:45:23 +0100

    Use nodejs 18 and mongo 6.0 for open5gs v2.5.6

--- mongo/mongo_init.sh
+++ mongo/mongo_init.sh
@@ -29,4 +29,4 @@
 # Sync docker time
 #ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-mongod --smallfiles --dbpath /var/lib/mongodb --logpath /var/log/mongodb/mongodb.log --bind_ip 0.0.0.0
+mongod --dbpath /var/lib/mongodb --logpath /var/log/mongodb/mongodb.log --bind_ip 0.0.0.0

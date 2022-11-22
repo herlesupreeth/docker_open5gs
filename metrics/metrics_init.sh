b5dfab1da339234ac0b39034e180f39f20e0ffa8
herlesupreeth <herlesupreeth@gmail.com>
2022-11-22 10:22:09 +0100

    Add prometheus to deployment and integrate with open5gs mme, amf and smf metrics server

--- metrics/metrics_init.sh
+++ metrics/metrics_init.sh
@@ -29,4 +29,12 @@
 # Sync docker time
 #ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-mongod --dbpath /var/lib/mongodb --logpath /var/log/mongodb/mongodb.log --bind_ip 0.0.0.0
+mkdir -p /config
+
+cp /mnt/metrics/prometheus.yml /config/
+
+sed -i 's|AMF_IP|'$AMF_IP'|g' /config/prometheus.yml
+sed -i 's|SMF_IP|'$SMF_IP'|g' /config/prometheus.yml
+sed -i 's|MME_IP|'$MME_IP'|g' /config/prometheus.yml
+
+./prometheus --config.file=/config/prometheus.yml

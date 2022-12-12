9aad7db4b41eaa16a224c0a16ce1b5ccd28d172a
herlesupreeth <herlesupreeth@gmail.com>
2022-12-12 09:53:29 +0100

    Add metrics config for PCF and UPF

--- metrics/metrics_init.sh
+++ metrics/metrics_init.sh
@@ -36,5 +36,7 @@ cp /mnt/metrics/prometheus.yml /config/
 sed -i 's|AMF_IP|'$AMF_IP'|g' /config/prometheus.yml
 sed -i 's|SMF_IP|'$SMF_IP'|g' /config/prometheus.yml
 sed -i 's|MME_IP|'$MME_IP'|g' /config/prometheus.yml
+sed -i 's|PCF_IP|'$PCF_IP'|g' /config/prometheus.yml
+sed -i 's|UPF_IP|'$UPF_IP'|g' /config/prometheus.yml
 
 ./prometheus --config.file=/config/prometheus.yml

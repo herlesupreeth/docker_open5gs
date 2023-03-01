428af8efc35fe52650f9d51a43a56d63fcba959e
herlesupreeth <herlesupreeth@gmail.com>
2023-03-01 08:35:41 +0100

    Allow direct communication with NRF when SCP is absent

--- bsf/bsf_init.sh
+++ bsf/bsf_init.sh
@@ -31,6 +31,7 @@ export DB_URI="mongodb://${MONGO_IP}/open5gs"
 cp /mnt/bsf/bsf.yaml install/etc/open5gs
 sed -i 's|BSF_IP|'$BSF_IP'|g' install/etc/open5gs/bsf.yaml
 sed -i 's|SCP_IP|'$SCP_IP'|g' install/etc/open5gs/bsf.yaml
+sed -i 's|NRF_IP|'$NRF_IP'|g' install/etc/open5gs/bsf.yaml
 sed -i 's|MONGO_IP|'$MONGO_IP'|g' install/etc/open5gs/bsf.yaml
 
 # Sync docker time

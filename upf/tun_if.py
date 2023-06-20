4f570c0a655b58d6d7ae6e76e07754351bef35d5
b3c3ll <131258572+b3c3ll@users.noreply.github.com>
2023-06-20 08:47:20 +0330

    Introducing UE_IPV4_INTERNET/IMS variable (#181)
    * Introducing UE_IPV4_INTERNET/IMS variable
    
    {Added} Introducing UE_IPV4_INTERNET/IMS variable and some minor changes to readme and DOCKER_HOST_IP
    
    * Introducing UE_IPV4_INTERNET/IMS variable
    
    Fixing Requested changes

--- upf/tun_if.py
+++ upf/tun_if.py
@@ -34,68 +34,75 @@ e.g:
 $ python3 tun_if.py --tun_ifname ogstun --ipv4_range 192.168.100.0/24 --ipv6_range 2001:230:cafe::/48
 """
 
+
 def validate_ip_net(ctx, param, value):
-	try:
-		ip_net = ipaddress.ip_network(value)
-		return ip_net
-	except ValueError:
-		raise click.BadParameter('Value does not represent a valid IPv4/IPv6 range')
+    try:
+        ip_net = ipaddress.ip_network(value)
+        return ip_net
+    except ValueError:
+        raise click.BadParameter(
+            'Value does not represent a valid IPv4/IPv6 range')
+
 
 @click.command()
 @click.option('--tun_ifname',
-			required=True,
-			help='TUN interface name e.g. ogstun')
+              required=True,
+              help='TUN interface name e.g. ogstun')
 @click.option('--ipv4_range',
-			required=True,
-			callback=validate_ip_net,
-			help='UE IPv4 Address range in CIDR format e.g. 192.168.100.0/24')
+              required=True,
+              callback=validate_ip_net,
+              help='UE IPv4 Address range in CIDR format e.g. 192.168.100.0/24')
 @click.option('--ipv6_range',
-			required=True,
-			callback=validate_ip_net,
-			help='UE IPv6 Address range in CIDR format e.g. 2001:230:cafe::/48')
+              required=True,
+              callback=validate_ip_net,
+              help='UE IPv6 Address range in CIDR format e.g. 2001:230:cafe::/48')
 @click.option('--nat_rule',
-			default='yes',
-			help='Option specifying whether to add NATing iptables rule or not')
+              default='yes',
+              help='Option specifying whether to add NATing iptables rule or not')
 def start(tun_ifname,
-		  ipv4_range,
-		  ipv6_range,
-		  nat_rule):
+          ipv4_range,
+          ipv6_range,
+          nat_rule):
 
-	# Get the first IP address in the IP range and netmask prefix length
-	first_ipv4_addr = next(ipv4_range.hosts(), None)
-	if not first_ipv4_addr:
-		raise ValueError('Invalid UE IPv4 range. Only one IP given')
-	else:
-		first_ipv4_addr = first_ipv4_addr.exploded
-	first_ipv6_addr = next(ipv6_range.hosts(), None)
-	if not first_ipv6_addr:
-		raise ValueError('Invalid UE IPv6 range. Only one IP given')
-	else:
-		first_ipv6_addr = first_ipv6_addr.exploded
+    # Get the first IP address in the IP range and netmask prefix length
+    first_ipv4_addr = next(ipv4_range.hosts(), None)
+    if not first_ipv4_addr:
+        raise ValueError('Invalid UE IPv4 range. Only one IP given')
+    else:
+        first_ipv4_addr = first_ipv4_addr.exploded
+    first_ipv6_addr = next(ipv6_range.hosts(), None)
+    if not first_ipv6_addr:
+        raise ValueError('Invalid UE IPv6 range. Only one IP given')
+    else:
+        first_ipv6_addr = first_ipv6_addr.exploded
 
-	ipv4_netmask_prefix = ipv4_range.prefixlen
-	ipv6_netmask_prefix = ipv6_range.prefixlen
+    ipv4_netmask_prefix = ipv4_range.prefixlen
+    ipv6_netmask_prefix = ipv6_range.prefixlen
 
-	# Setup the TUN interface, set IP address and setup IPtables
-	# if ls /sys/class/net | grep "ogstun" ; then ip link delete ogstun; fi
-	execute_bash_cmd('ip tuntap add name ' + tun_ifname + ' mode tun')
-	execute_bash_cmd('ip addr add ' + first_ipv4_addr + '/' + str(ipv4_netmask_prefix) + ' dev ' + tun_ifname)
-	execute_bash_cmd('ip addr add ' + first_ipv6_addr + '/' + str(ipv6_netmask_prefix) + ' dev ' + tun_ifname)
-	execute_bash_cmd('ip link set ' + tun_ifname + ' mtu 1450')
-	execute_bash_cmd('ip link set ' + tun_ifname + ' up')
-	if nat_rule == 'yes':
-		execute_bash_cmd('if ! iptables-save | grep -- \"-A POSTROUTING -s ' + ipv4_range.with_prefixlen + ' ! -o ' + tun_ifname + ' -j MASQUERADE\" ; then ' +
-			'iptables -t nat -A POSTROUTING -s ' + ipv4_range.with_prefixlen + ' ! -o ' + tun_ifname + ' -j MASQUERADE; fi')
-		execute_bash_cmd('if ! ip6tables-save | grep -- \"-A POSTROUTING -s ' + ipv6_range.with_prefixlen + ' ! -o ' + tun_ifname + ' -j MASQUERADE\" ; then ' +
-			'ip6tables -t nat -A POSTROUTING -s ' + ipv6_range.with_prefixlen + ' ! -o ' + tun_ifname + ' -j MASQUERADE; fi')
-		execute_bash_cmd('if ! iptables-save | grep -- \"-A INPUT -i ' + tun_ifname + ' -j ACCEPT\" ; then ' +
-			'iptables -A INPUT -i ' + tun_ifname + ' -j ACCEPT; fi')
-		execute_bash_cmd('if ! ip6tables-save | grep -- \"-A INPUT -i ' + tun_ifname + ' -j ACCEPT\" ; then ' +
-			'ip6tables -A INPUT -i ' + tun_ifname + ' -j ACCEPT; fi')
+    # Setup the TUN interface, set IP address and setup IPtables
+    # if ls /sys/class/net | grep "ogstun" ; then ip link delete ogstun; fi
+    execute_bash_cmd('ip tuntap add name ' + tun_ifname + ' mode tun')
+    execute_bash_cmd('ip addr add ' + first_ipv4_addr + '/' +
+                     str(ipv4_netmask_prefix) + ' dev ' + tun_ifname)
+    execute_bash_cmd('ip addr add ' + first_ipv6_addr + '/' +
+                     str(ipv6_netmask_prefix) + ' dev ' + tun_ifname)
+    execute_bash_cmd('ip link set ' + tun_ifname + ' mtu 1450')
+    execute_bash_cmd('ip link set ' + tun_ifname + ' up')
+    if nat_rule == 'yes':
+        execute_bash_cmd('if ! iptables-save | grep -- \"-A POSTROUTING -s ' + ipv4_range.with_prefixlen + ' ! -o ' + tun_ifname + ' -j MASQUERADE\" ; then ' +
+                         'iptables -t nat -A POSTROUTING -s ' + ipv4_range.with_prefixlen + ' ! -o ' + tun_ifname + ' -j MASQUERADE; fi')
+        execute_bash_cmd('if ! ip6tables-save | grep -- \"-A POSTROUTING -s ' + ipv6_range.with_prefixlen + ' ! -o ' + tun_ifname + ' -j MASQUERADE\" ; then ' +
+                         'ip6tables -t nat -A POSTROUTING -s ' + ipv6_range.with_prefixlen + ' ! -o ' + tun_ifname + ' -j MASQUERADE; fi')
+        execute_bash_cmd('if ! iptables-save | grep -- \"-A INPUT -i ' + tun_ifname + ' -j ACCEPT\" ; then ' +
+                         'iptables -A INPUT -i ' + tun_ifname + ' -j ACCEPT; fi')
+        execute_bash_cmd('if ! ip6tables-save | grep -- \"-A INPUT -i ' + tun_ifname + ' -j ACCEPT\" ; then ' +
+                         'ip6tables -A INPUT -i ' + tun_ifname + ' -j ACCEPT; fi')
+
 
 def execute_bash_cmd(bash_cmd):
-	#print("Executing: /bin/bash -c " + bash_cmd)
-	return subprocess.run(bash_cmd, stdout=subprocess.PIPE, shell=True)
+    # print("Executing: /bin/bash -c " + bash_cmd)
+    return subprocess.run(bash_cmd, stdout=subprocess.PIPE, shell=True)
+
 
 if __name__ == '__main__':
-	start()
+    start()
--- upf/upf_init.sh
+++ upf/upf_init.sh
@@ -31,12 +31,14 @@ export LANG=C.UTF-8
 export IP_ADDR=$(awk 'END{print $1}' /etc/hosts)
 export IF_NAME=$(ip r | awk '/default/ { print $5 }')
 
-python3 /mnt/upf/tun_if.py --tun_ifname ogstun --ipv4_range 192.168.100.0/24 --ipv6_range 2001:230:cafe::/48
-python3 /mnt/upf/tun_if.py --tun_ifname ogstun2 --ipv4_range 192.168.101.0/24 --ipv6_range 2001:230:babe::/48 --nat_rule 'no'
+python3 /mnt/upf/tun_if.py --tun_ifname ogstun --ipv4_range $UE_IPV4_INTERNET --ipv6_range 2001:230:cafe::/48
+python3 /mnt/upf/tun_if.py --tun_ifname ogstun2 --ipv4_range $UE_IPV4_IMS --ipv6_range 2001:230:babe::/48 --nat_rule 'no'
 
 cp /mnt/upf/upf.yaml install/etc/open5gs
 sed -i 's|UPF_IP|'$UPF_IP'|g' install/etc/open5gs/upf.yaml
 sed -i 's|SMF_IP|'$SMF_IP'|g' install/etc/open5gs/upf.yaml
+sed -i 's|UE_IPV4_INTERNET|'$UE_IPV4_INTERNET'|g' install/etc/open5gs/upf.yaml
+sed -i 's|UE_IPV4_IMS|'$UE_IPV4_IMS'|g' install/etc/open5gs/upf.yaml
 sed -i 's|UPF_ADVERTISE_IP|'$UPF_ADVERTISE_IP'|g' install/etc/open5gs/upf.yaml
 
 # Sync docker time

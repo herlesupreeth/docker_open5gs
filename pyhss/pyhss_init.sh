2b2c48ba69c1b221b5f1afdd962303040e232db6
herlesupreeth <herlesupreeth@gmail.com>
2024-02-09 10:25:20 +0100

    Use latest commits from pyHSS

--- pyhss/pyhss_init.sh
+++ pyhss/pyhss_init.sh
@@ -51,17 +51,28 @@ cp /mnt/pyhss/config.yaml ./
 cp /mnt/pyhss/default_ifc.xml ./
 cp /mnt/pyhss/default_sh_user_data.xml ./
 
+INSTALL_PREFIX="/pyhss"
+
 sed -i 's|PYHSS_IP|'$PYHSS_IP'|g' ./config.yaml
 sed -i 's|PYHSS_BIND_PORT|'$PYHSS_BIND_PORT'|g' ./config.yaml
 sed -i 's|IMS_DOMAIN|'$IMS_DOMAIN'|g' ./config.yaml
 sed -i 's|OP_MCC|'$MCC'|g' ./config.yaml
 sed -i 's|OP_MNC|'$MNC'|g' ./config.yaml
 sed -i 's|MYSQL_IP|'$MYSQL_IP'|g' ./config.yaml
+sed -i 's|INSTALL_PREFIX|'$INSTALL_PREFIX'|g' ./config.yaml
 
 # Sync docker time
 #ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-export FLASK_APP=PyHSS_API.py
-flask run --host=$PYHSS_IP --port=8080 &
+git apply /mnt/pyhss/database_fix.patch
 
-python3 hss.py
+redis-server --daemonize yes
+
+cd services
+python3 apiService.py --host=$PYHSS_IP --port=8080 &
+# Sleep is needed to let db be populated in a non-overlapping fashion
+sleep 5
+python3 diameterService.py &
+# Sleep is needed to let db be populated in a non-overlapping fashion
+sleep 5
+python3 hssService.py

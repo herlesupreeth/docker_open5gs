857042c49f57f6c352c886d1337915fd8be3c87f
herlesupreeth <herlesupreeth@gmail.com>
2024-07-24 18:01:37 +0200

    Add HTTP2 server and HTTP client module initialization for N5 support

--- pcscf/pcscf_init.sh
+++ pcscf/pcscf_init.sh
@@ -70,8 +70,6 @@ fi
 if [[ ${DEPLOY_MODE} == 5G ]];
 then
     sed -i 's|#!define WITH_RX\b|##!define WITH_RX|g' /etc/kamailio_pcscf/pcscf.cfg
-    sed -i 's|#!define WITH_RX_REG\b|##!define WITH_RX_REG|g' /etc/kamailio_pcscf/pcscf.cfg
-    sed -i 's|#!define WITH_RX_CALL\b|##!define WITH_RX_CALL|g' /etc/kamailio_pcscf/pcscf.cfg
 fi
 
 sed -i 's|PCSCF_IP|'$PCSCF_IP'|g' /etc/kamailio_pcscf/pcscf.cfg

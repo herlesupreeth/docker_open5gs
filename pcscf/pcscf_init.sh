220357e8e679cbf0fe3af581bfd5c7098fe78832
herlesupreeth <herlesupreeth@gmail.com>
2024-08-11 17:59:22 +0200

    Enable N5 interface only in 5G deployment

--- pcscf/pcscf_init.sh
+++ pcscf/pcscf_init.sh
@@ -70,6 +70,7 @@ fi
 if [[ ${DEPLOY_MODE} == 5G ]];
 then
     sed -i 's|#!define WITH_RX\b|##!define WITH_RX|g' /etc/kamailio_pcscf/pcscf.cfg
+	sed -i 's|##!define WITH_N5\b|#!define WITH_N5|g' /etc/kamailio_pcscf/pcscf.cfg
 fi
 
 sed -i 's|PCSCF_IP|'$PCSCF_IP'|g' /etc/kamailio_pcscf/pcscf.cfg

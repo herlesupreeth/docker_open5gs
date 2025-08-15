a0ca163ba4606d9c73bdbf76ae8d063030e737fb
herlesupreeth <herlesupreeth@gmail.com>
2025-08-15 21:22:47 +0200

    Use debian bookworm image for rtpengine

--- rtpengine/rtpengine_init.sh
+++ rtpengine/rtpengine_init.sh
@@ -58,31 +58,6 @@ fi
 #ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
 set +e
-if [ -e /proc/rtpengine/control ]; then
-	echo "del $TABLE" > /proc/rtpengine/control 2>/dev/null
-fi
-# Freshly add the iptables rules to forward the udp packets to the iptables-extension "RTPEngine":
-# Remember iptables table = chains, rules stored in the chains
-# -N (create a new chain with the name rtpengine)
-iptables -N rtpengine 2> /dev/null
-
-# -D: Delete the rule for the target "rtpengine" if exists. -j (target): chain name or extension name
-# from the table "filter" (the default -without the option '-t')
-iptables -D INPUT -j rtpengine 2> /dev/null
-# Add the rule again so the packets will go to rtpengine chain after the (filter-INPUT) hook point.
-iptables -I INPUT -j rtpengine
-# Delete and Insert a rule in the rtpengine chain to forward the UDP traffic
-iptables -D rtpengine -p udp -j RTPENGINE --id "$TABLE" 2>/dev/null
-iptables -I rtpengine -p udp -j RTPENGINE --id "$TABLE"
-iptables-save > /etc/iptables.rules
-
-# The same for IPv6
-ip6tables -N rtpengine 2> /dev/null
-ip6tables -D INPUT -j rtpengine 2> /dev/null
-ip6tables -I INPUT -j rtpengine
-ip6tables -D rtpengine -p udp -j RTPENGINE --id "$TABLE" 2>/dev/null
-ip6tables -I rtpengine -p udp -j RTPENGINE --id "$TABLE"
-ip6tables-save > /etc/ip6tables.rules
 
 # Add static route to route traffic back to UE as there is not NATing
 ip r add ${UE_IPV4_IMS} via ${UPF_IP}
